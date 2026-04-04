b13e951df19f0146b34c01da0c0ac14f713cc8c1
Rahma Djebbi <rahmadjebbi67@gmail.com>
2026-04-04 20:47:53 +0100

    1ere version + Front office

---
+++ src/Controller/FrontProduitController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProduitRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FrontProduitController extends AbstractController
+{
+    #[Route('/boutique', name: 'front_produit_index')]
+    public function index(ProduitRepository $repository, Request $request): Response
+    {
+        // Remplacement de searchField.getText()
+        $searchTerm = $request->query->get('q', '');
+
+        if ($searchTerm) {
+            $produits = $repository->findBySearch($searchTerm);
+        } else {
+            $produits = $repository->findAll();
+        }
+
+        // Remplacement de mettreAJourStatistiques()
+        $total = count($produits);
+        $disponibles = 0;
+        foreach ($produits as $p) {
+            if ($p->getStockActuel() > 0) {
+                $disponibles++;
+            }
+        }
+
+        return $this->render('front/produit/index.html.twig', [
+            'produits' => $produits,
+            'searchTerm' => $searchTerm,
+            'stats' => [
+                'total' => $total,
+                'disponibles' => $disponibles
+            ]
+        ]);
+    }
+}
