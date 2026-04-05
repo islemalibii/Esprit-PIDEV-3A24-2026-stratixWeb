948e96f2a24df20885e78334e4de706af1f5b24a
islem <islem.alibii@gmail.com>
2026-04-05 14:18:09 +0100

    imports fixed

--- src/Form/EvenementType.php
+++ src/Form/EvenementType.php
@@ -9,8 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
+
 
 
 class EvenementType extends AbstractType
