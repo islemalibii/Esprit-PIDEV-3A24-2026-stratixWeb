e9e0704c444667542947965a0610b29e0ef6770a
Rahma Djebbi <rahmadjebbi67@gmail.com>
2026-04-17 20:07:51 +0100

    ajout du code de fonctionnement de commande php bin/console app:fetch-emails

--- src/Command/FetchEmailsCommand.php
+++ src/Command/FetchEmailsCommand.php
@@ -2,48 +2,130 @@
 
 namespace App\Command;
 
+use PhpImap\Mailbox;
+use App\Repository\FournisseurRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Offre;
+use App\Repository\RessourceRepository;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 #[AsCommand(
     name: 'app:fetch-emails',
-    description: 'Add a short description for your command',
+    description: 'Récupère les catalogues CSV des fournisseurs par email',
 )]
 class FetchEmailsCommand extends Command
 {
-    public function __construct()
+    private $params;
+    private $fournisseurRepo;
+    private $ressourceRepo;
+    private $em;
+
+    public function __construct(ParameterBagInterface $params, FournisseurRepository $fournisseurRepo, RessourceRepository $ressourceRepo, EntityManagerInterface $em)
     {
         parent::__construct();
-    }
-
-    protected function configure(): void
-    {
-        $this
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
-        ;
+        $this->params = $params;
+        $this->fournisseurRepo = $fournisseurRepo;
+        $this->ressourceRepo = $ressourceRepo;
+        $this->em = $em;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
+        $uploadDir = $this->params->get('catalogues_directory');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
+        // 1. CONFIGURATION MAILBOX (À adapter avec tes accès Gmail/Outlook)
+        // Format: {serveur:port/imap/ssl}Dossier
+        $mailbox = new Mailbox(
+            '{imap.gmail.com:993/imap/ssl}INBOX', 
+            'ton-email-stratix@gmail.com', 
+            'ton-mot-de-passe-application'
+        );
+
+        try {
+            // 2. CHERCHER LES EMAILS NON LUS
+            $mailsIds = $mailbox->searchMailbox('UNSEEN');
+
+            if (empty($mailsIds)) {
+                $io->info('Aucun nouvel email trouvé.');
+                return Command::SUCCESS;
+            }
+
+            foreach ($mailsIds as $mailId) {
+                $mail = $mailbox->getMail($mailId);
+                $senderEmail = $mail->fromAddress;
+
+                // 3. VÉRIFIER SI L'EXPÉDITEUR EST UN FOURNISSEUR ENREGISTRÉ
+                $fournisseur = $this->fournisseurRepo->findOneBy(['email' => $senderEmail]);
+
+                if (!$fournisseur) {
+                    $io->warning("Email reçu de $senderEmail, mais ce n'est pas un fournisseur enregistré. Ignoré.");
+                    continue;
+                }
+
+                // 4. RÉCUPÉRER LES PIÈCES JOINTES (CSV)
+                $attachments = $mail->getAttachments();
+                foreach ($attachments as $attachment) {
+                    if (strtolower(pathinfo($attachment->name, PATHINFO_EXTENSION)) === 'csv') {
+                        
+                        $filePath = $uploadDir . '/' . uniqid() . '_' . $attachment->name;
+                        
+                        // Sauvegarde physique du fichier
+                        if ($attachment->saveTo($filePath)) {
+                            $io->note("Fichier CSV enregistré : " . $attachment->name);
+                            
+                            // 5. ANALYSE ET IMPORTATION AUTOMATIQUE
+                            $this->importCsvData($filePath, $fournisseur, $io);
+                        }
+                    }
+                }
+                
+                // Marquer comme lu
+                $mailbox->markMailAsRead($mailId);
+            }
+
+            $io->success('Récupération et importation terminées !');
+            return Command::SUCCESS;
+
+        } catch (\Exception $e) {
+            $io->error('Erreur lors de la connexion IMAP : ' . $e->getMessage());
+            return Command::FAILURE;
         }
+    }
 
-        if ($input->getOption('option1')) {
-            // ...
+    private function importCsvData(string $filePath, $fournisseur, SymfonyStyle $io): void
+    {
+        if (($handle = fopen($filePath, "r")) !== FALSE) {
+            // On saute la première ligne si c'est une entête
+            fgetcsv($handle, 1000, ","); 
+
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                // Supposons le format CSV : NomRessource, Prix, Delai
+                $nomRessource = $data[0];
+                $prix = (float)$data[1];
+                $delai = (int)$data[2];
+
+                $ressource = $this->ressourceRepo->findOneBy(['nom' => $nomRessource]);
+
+                if ($ressource) {
+                    $offre = new Offre();
+                    $offre->setFournisseur($fournisseur);
+                    $offre->setRessource($ressource);
+                    $offre->setPrix($prix);
+                    $offre->setDelaiTransportJours($delai);
+                    $offre->setDateOffre(new \DateTime());
+
+                    $this->em->persist($offre);
+                }
+            }
+            $this->em->flush();
+            fclose($handle);
+            $io->success("Les données de " . $fournisseur->getNom() . " ont été injectées en base.");
         }
-
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
-
-        return Command::SUCCESS;
     }
 }
